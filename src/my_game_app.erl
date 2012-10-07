7e00d35d823ed99379e4242b4a46534070bc8f94
Matthew Miller <matthewmillerinc@gmail.com>
2012-10-07 11:23:46 -0500

    setup initial route & started cowboy

--- src/my_game_app.erl
+++ src/my_game_app.erl
@@ -1,16 +1,25 @@
 -module(my_game_app).
-
 -behaviour(application).
 
-%% Application callbacks
--export([start/2, stop/1]).
+-export([start/0, start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
+start() ->
+  reloader:start(), %Makes debugging simple...
+  ok = application:start(cowboy),
+  ok = ssl:start(),
+  ok = inets:start(),  
+  ok = application:start(my_game).
 
 start(_StartType, _StartArgs) ->
-    my_game_sup:start_link().
+ Dispatch = [
+  {'_', [
+   {[<<"account">>, email], account_handler, []}
+  ]}
+ ],
+ {ok, _} = cowboy:start_listener(http, 100,
+   cowboy_tcp_transport, [{port, 8080}],
+   cowboy_http_protocol, [{dispatch, Dispatch}]
+  ),
+ my_game_sup:start_link().
 
-stop(_State) ->
-    ok.
+stop(_State) -> ok.
